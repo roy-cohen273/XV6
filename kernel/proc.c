525388958a9609b8a50be3856ab2fd742fe6bc78
Roy Cohen <roycohen273@gmail.com>
2024-03-03 21:06:04 +0200

    speed up system calls

--- kernel/proc.c
+++ kernel/proc.c
@@ -202,6 +202,18 @@ proc_pagetable(struct proc *p)
     return 0;
   }
 
+  // allocate map the usyscall page
+  struct usyscall *usyscall = kalloc();
+  if (usyscall != 0) {
+    usyscall->pid = p->pid;
+  }
+  if (usyscall == 0 || mappages(pagetable, USYSCALL, PGSIZE, (uint64)usyscall, PTE_R | PTE_U) < 0) {
+    uvmunmap(pagetable, TRAMPOLINE, 1, 0);
+    uvmunmap(pagetable, TRAPFRAME, 1, 0);
+    uvmfree(pagetable, 0);
+    return 0;
+  }
+
   return pagetable;
 }
 
@@ -212,6 +224,7 @@ proc_freepagetable(pagetable_t pagetable, uint64 sz)
 {
   uvmunmap(pagetable, TRAMPOLINE, 1, 0);
   uvmunmap(pagetable, TRAPFRAME, 1, 0);
+  uvmunmap(pagetable, USYSCALL, 1, 1);
   uvmfree(pagetable, sz);
 }
 
