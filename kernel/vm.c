6f56b8226788d0fd6380b90090524acfda4bc31a
Roy Cohen <roycohen273@gmail.com>
2024-03-03 21:21:55 +0200

    print a page table

--- kernel/vm.c
+++ kernel/vm.c
@@ -437,3 +437,31 @@ copyinstr(pagetable_t pagetable, char *dst, uint64 srcva, uint64 max)
     return -1;
   }
 }
+
+// Print pagetable entries
+void
+print_entries(pagetable_t pagetable, int level)
+{
+  for (int i = 0; i < 512; i++) {
+    pte_t pte = pagetable[i];
+    if ((pte & PTE_V) == 0) {
+      continue;
+    }
+    for (int j = 0; j < level; j++) {
+      printf(" ..");
+    }
+    printf("%d: pte %p pa %p\n", i, pte, PTE2PA(pte));
+    if ((pte & (PTE_R|PTE_W|PTE_X)) == 0) {
+      pagetable_t child = (pagetable_t)PTE2PA(pte);
+      print_entries(child, level + 1);
+    }
+  }
+}
+
+// Print pagetable information.
+void
+vmprint(pagetable_t pagetable)
+{
+  printf("page table %p\n", pagetable);
+  print_entries(pagetable, 1);
+}
